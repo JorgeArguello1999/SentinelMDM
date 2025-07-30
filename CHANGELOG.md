6102cafcc885723aa0c1188aaf089d5ed5cb6e4a
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-07-29 20:41:34 -0500

    feat: Update documentation and fix typos

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Freatures that I need now
+
+## Version `0.1.0`
+[ x ] I want to do monitoring my websites for check good working, this alert send me by Telegram 
--- main.py
+++ main.py
@@ -5,12 +5,36 @@ from dotenv import load_dotenv
 from os import getenv
 load_dotenv()
 
-app = FastAPI()
+from pydantic import BaseModel
+from fastapi import status, HTTPException
+
+class ResponseModel(BaseModel):
+    message: str
+
+app = FastAPI(
+    title = "Sentinel Backend API",
+    description = "API for managing users, authentication, agents and others functions.",
+    version = "0.1.0", # Update this version as needed
+    contact = {
+        "name": "JorgeArguello1999",
+        "email": "contact@jorgearguello.net",
+    },
+    license_info = {
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    }
+)
 
 # Registering routes
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 
-@app.get("/")
+@app.get("/", tags=["Root"], response_model=ResponseModel, 
+    responses={
+        status.HTTP_200_OK: {"description": "Root endpoint"},
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"description": "Internal Server Error"},
+        status.HTTP_404_NOT_FOUND: {"description": "Not Found"}
+    }
+)
 def read_root():
     return {"message": "Welcome to the FastAPI project!"}
 
